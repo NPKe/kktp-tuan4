190c68d194c9b58ac7bdc11cf628b2c05e8597a1
kenguyen276 <kenguyen276@gmail.com>
2016-03-22 22:44:34 +0700

    Bai tap tuan4

---
+++ DesignPattern/Profile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    class Profile
+    {
+        protected int maxDownloadTimes;
+        protected int maxConnections;
+        protected int DownloadedTimes;
+
+        public bool CheckCanDownload()
+        {
+            if (DownloadedTimes < maxDownloadTimes)
+                return true;
+            return false;
+        }
+
+        public int CheckDownloadedTimes()
+        {
+            return DownloadedTimes;
+        }
+
+        public bool CheckValidDownloadConnection(int numOfConnection)
+        {
+            if (numOfConnection <= maxConnections)
+                return true;
+            return false;
+        }
+    }
+
+    class Basic : Profile
+    {
+        public Basic()
+        {
+            maxConnections = 1;
+            DownloadedTimes = 0;
+            maxDownloadTimes = 1;
+        }
+    }
+
+    class VIP : Profile
+    {
+        public VIP()
+        {
+            maxConnections = 20;
+            DownloadedTimes = -1;
+            maxDownloadTimes = 1;
+        }
+    }
+}
---
+++ DesignPattern/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    enum ProfileType { Basic, VIP };
+    class User
+    {
+        public int ID { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        private Profile profile;
+
+        private ProfileType profileType;
+
+        public bool CheckCanDownload()
+        {
+            return profile.CheckCanDownload();
+        }
+
+        public bool CheckNumberOfValidConnection(int numOfConnection)
+        {
+            return profile.CheckValidDownloadConnection(numOfConnection);
+        }
+
+        public void Upgrade()
+        {
+            if (profileType == ProfileType.Basic)
+                profile = new VIP();
+        }
+
+        public void Downgrade()
+        {
+            if (profileType == ProfileType.VIP)
+                profile = new Basic();
+        }
+    }
+}
